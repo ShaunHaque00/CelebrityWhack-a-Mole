b821ee6b39574f1e49dbfc9020b1931ebaf46539
mohammadhaque <mohammadhaque106@yahoo.com>
2016-01-18 17:08:47 -0500

    update

---
+++ CelebrityWhack-a-Mole/game.h
@@ -0,0 +1,41 @@
+//
+//  game.h
+//  CelebrityWhack-a-Mole
+//
+//  Created by Mohammad Haque on 1/18/16.
+//  Copyright © 2016 shaun Haque. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface game : UIViewController {
+    
+    
+    __weak IBOutlet UIButton *box1;
+    
+    __weak IBOutlet UIButton *box2;
+    
+    __weak IBOutlet UIButton *box3;
+    
+    __weak IBOutlet UIButton *box4;
+
+    __weak IBOutlet UIButton *box5;
+
+    __weak IBOutlet UIButton *box6;
+
+    __weak IBOutlet UIButton *box7;
+
+    __weak IBOutlet UIButton *box8;
+
+    __weak IBOutlet UIButton *box9;
+    
+    
+    
+    
+    
+    
+    
+}
+
+@end
---
+++ CelebrityWhack-a-Mole/game.m
@@ -0,0 +1,28 @@
+//
+//  game.m
+//  CelebrityWhack-a-Mole
+//
+//  Created by Mohammad Haque on 1/18/16.
+//  Copyright © 2016 shaun Haque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "game.h"
+
+@interface game ()
+
+@end
+
+@implementation game 
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
